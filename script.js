6066a4b6da5bab05c1887bc4f542a716e976fa30
biggestegg566 <159206344+biggestegg566@users.noreply.github.com>
2024-03-13 15:04:29 +1100

    artwork

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let entername = prompt("entername");
+let playerGuess;
+//let computerGuess = randomNum
+let computerGuess = randomnum()
+let playerFeedback ="test";
 loadDIV ();
-
-
-
-function loadDIV() {
-
+function randomnum(){
+    return Math.floor(Math.random() * (10 - 1 + 1)+1)
 }
+function loadDIV() {}
 document.getElementById("ScriptThisDIV").innerHTML = "<H1>hello " + entername+ "</H1>"
 document.getElementById("ScriptThisDIV").innerHTML = "<H1>hello" + entername + "</H1><H2>Guess a Number"
 document.getElementById("ScriptThisDIV").innerHTML = "<H1>hello" + entername + "</H1><H2>Guess a Number" + playFeedback
